b3455589bad0c023223f225e95b5c122e2dde7c3
Cindy Villanueva <cindy.villanueva.ds@gmail.com>
2021-03-24 11:57:58 -0500

    correct one question

--- select_exercises.sql
+++ select_exercises.sql
@@ -4,7 +4,8 @@ USE albums_db;
 -- 3. Explore the structure of the albums table.
 DESCRIBE albums;
 
---		a. How many rows are in the albums table? 6 rows
+--		a. How many rows are in the albums table? 31
+SELECT * FROM albums;
 -- 		b. How many unique artist names are in the albums table? 23
 SELECT DISTINCT artist FROM albums;
 
@@ -17,6 +18,9 @@ SELECT MAX(release_date) FROM albums;
 -- oldest release 
 SELECT MIN(release_date) FROM albums; 
 
+SELECT DISTINCT release_date FROM albums
+ORDER BY release_date;
+
 -- 4.a. The name of all albums by Pink Floyd
 SELECT name, artist FROM albums
 WHERE artist = 'Pink Floyd';
@@ -24,17 +28,21 @@ WHERE artist = 'Pink Floyd';
 -- 4b. The year Sgt. Pepper's Lonely Hearts Club Band was released. 1967
 SELECT  release_date FROM albums 
 WHERE name = "Sgt. Pepper's Lonely Hearts Club Band";
+SELECT  release_date FROM albums 
+WHERE name = 'Sgt. Pepper\'s Lonely Hearts Club Band'; -- use \ to ignore ';
 
--- 4c. The genre for the album Nevermind. Grunge, Alternative rock.
+
+
+-- 4c. The genre for the album Nevermind. Grunge, Alternative rock. 
 SELECT genre  FROM albums
 WHERE name = "Nevermind";
 
--- 4d. Which albums were released in the 1990s
+-- 4d. Which albums were released in the 1990s (returns 11 records)
 SELECT name 
 FROM albums
 WHERE release_date BETWEEN 1990 AND 1999;
 
--- 4e. Which albums had less than 20 million certified sales. 
+-- 4e. Which albums had less than 20 million certified sales. (returns 13 records)
 SELECT name, sales FROM albums
 WHERE sales < 20;
 
@@ -46,4 +54,5 @@ WHERE genre ='Rock';
 -- the fields that have exact genre as Rock. whe can use LIKE to include albums with the word Rock in genre 
 SELECT name, genre FROM albums
 WHERE genre LIKE '%Rock%';
+
 
